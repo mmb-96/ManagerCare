c94e8523df2b40255024f68c00dac24bdf62eb01
Manuel Melero <mmb_96@hotmail.com>
2020-04-02 12:37:53 +0200

    Añadido la consulta para objetivos los objetivos de los usuarios.

--- src/main/java/org/jacaranda/ies/repository/ObjetivoRepository.java
+++ src/main/java/org/jacaranda/ies/repository/ObjetivoRepository.java
@@ -1,8 +1,10 @@
 package org.jacaranda.ies.repository;
 
-import org.jacaranda.ies.domain.Objetivo;
+import java.util.List;
 
-import org.springframework.data.jpa.repository.*;
+import org.jacaranda.ies.domain.Objetivo;
+import org.springframework.data.jpa.repository.JpaRepository;
+import org.springframework.data.jpa.repository.Query;
 import org.springframework.stereotype.Repository;
 
 /**
@@ -11,4 +13,7 @@ import org.springframework.stereotype.Repository;
 @SuppressWarnings("unused")
 @Repository
 public interface ObjetivoRepository extends JpaRepository<Objetivo, Long> {
+	
+    @Query("select o from Objetivo o join fetch o.categorias cat where cat.id = (Select eu.categoria from UserExtra eu where eu.user.login = ?#{principal.username})")
+    List<Objetivo> findObjetivoUser();
 }
