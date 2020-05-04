52d0609bc759aa9aa853c91faea6b43e8be50a6d
Manuel Melero <mmb_96@hotmail.com>
2020-05-04 14:09:17 +0200

    Se modifica la vista, para que tambien muestre los objetivos de empleado

--- src/main/webapp/app/entities/user-teams/user-teams-detail.component.ts
+++ src/main/webapp/app/entities/user-teams/user-teams-detail.component.ts
@@ -1,7 +1,10 @@
 import { Component, OnInit } from '@angular/core';
-import { ActivatedRoute } from '@angular/router';
+import { ActivatedRoute, Params } from '@angular/router';
 
 import { IUser } from 'app/core/user/user.model';
+import { IObjetivosConseguidos } from 'app/shared/model/objetivos-conseguidos.model';
+import { HttpResponse } from '@angular/common/http';
+import { UserTeamsService } from './user-teams.service';
 
 @Component({
   selector: 'jhi-user-teams-detail',
@@ -9,11 +12,22 @@ import { IUser } from 'app/core/user/user.model';
 })
 export class UserTeamsDetailComponent implements OnInit {
   userTeams: IUser | null = null;
+  objetivosConseguidos?: IObjetivosConseguidos[];
 
-  constructor(protected activatedRoute: ActivatedRoute) {}
+  constructor(protected activatedRoute: ActivatedRoute, private service: UserTeamsService) {}
+
+  loadAll(login: string): void {
+    this.service
+      .findObject(login)
+      .subscribe((res: HttpResponse<IObjetivosConseguidos[]>) => (this.objetivosConseguidos = res.body || []));
+  }
 
   ngOnInit(): void {
     this.activatedRoute.data.subscribe(({ userTeam }) => (this.userTeams = userTeam));
+    this.activatedRoute.params.forEach((params: Params) => {
+      const id = params['id'];
+      this.loadAll(id);
+    });
   }
 
   previousState(): void {
--- src/main/webapp/app/entities/user-teams/user-teams.service.ts
+++ src/main/webapp/app/entities/user-teams/user-teams.service.ts
@@ -5,14 +5,19 @@ import { Observable } from 'rxjs';
 import { SERVER_API_URL } from 'app/app.constants';
 import { createRequestOption } from 'app/shared/util/request-util';
 import { IUser } from 'app/core/user/user.model';
+import { map } from 'rxjs/operators';
+import * as moment from 'moment';
+import { IObjetivosConseguidos } from 'app/shared/model/objetivos-conseguidos.model';
 
 type EntityResponseType = HttpResponse<IUser>;
 type EntityArrayResponseType = HttpResponse<IUser[]>;
+type EntityArrayResponseTypeObject = HttpResponse<IObjetivosConseguidos[]>;
 
 @Injectable({ providedIn: 'root' })
 export class UserTeamsService {
   public resourceUrl = SERVER_API_URL + 'api/users-teams';
   public resourceUrlView = SERVER_API_URL + 'api/users';
+  public resourceOBjetivo = SERVER_API_URL + 'api/objetivos-ST';
 
   constructor(protected http: HttpClient) {}
 
@@ -20,8 +25,24 @@ export class UserTeamsService {
     return this.http.get<IUser>(`${this.resourceUrlView}/${login}`, { observe: 'response' });
   }
 
+  findObject(login: string): Observable<EntityArrayResponseTypeObject> {
+    return this.http
+    .get<IObjetivosConseguidos[]>(`${this.resourceOBjetivo}/${login}`, { observe: 'response' })
+    .pipe(map((res: EntityArrayResponseTypeObject) => this.convertDateArrayFromServer(res)));
+  }
+
   query(req?: any): Observable<EntityArrayResponseType> {
     const options = createRequestOption(req);
     return this.http.get<IUser[]>(this.resourceUrl, { params: options, observe: 'response' });
+  }
+
+  protected convertDateArrayFromServer(res: EntityArrayResponseTypeObject): EntityArrayResponseTypeObject {
+    if (res.body) {
+      res.body.forEach((objetivosConseguidos: IObjetivosConseguidos) => {
+        objetivosConseguidos.fechaApertura = objetivosConseguidos.fechaApertura ? moment(objetivosConseguidos.fechaApertura) : undefined;
+        objetivosConseguidos.fechaCierre = objetivosConseguidos.fechaCierre ? moment(objetivosConseguidos.fechaCierre) : undefined;
+      });
+    }
+    return res;
   }
 }
