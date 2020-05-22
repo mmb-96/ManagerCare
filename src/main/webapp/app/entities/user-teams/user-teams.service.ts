b123fff7a807e7487b5e67635cc50fd2ce3d5319
Manuel Melero <mmb_96@hotmail.com>
2020-05-22 16:52:26 +0200

    Front donde mostramos los puntos obtenidos desde el back end.

--- src/main/webapp/app/entities/user-teams/user-teams.service.ts
+++ src/main/webapp/app/entities/user-teams/user-teams.service.ts
@@ -8,16 +8,19 @@ import { IUser } from 'app/core/user/user.model';
 import { map } from 'rxjs/operators';
 import * as moment from 'moment';
 import { IObjetivosConseguidos } from 'app/shared/model/objetivos-conseguidos.model';
+import { IPuntosConseguidos } from 'app/shared/model/puntos-conseguidos.model';
 
 type EntityResponseType = HttpResponse<IUser>;
 type EntityArrayResponseType = HttpResponse<IUser[]>;
 type EntityArrayResponseTypeObject = HttpResponse<IObjetivosConseguidos[]>;
+type EntityArrayResponseTypePuntos = HttpResponse<IPuntosConseguidos[]>;
 
 @Injectable({ providedIn: 'root' })
 export class UserTeamsService {
   public resourceUrl = SERVER_API_URL + 'api/users-teams';
   public resourceUrlView = SERVER_API_URL + 'api/users';
   public resourceOBjetivo = SERVER_API_URL + 'api/objetivos-ST';
+  public resourcePuntos = SERVER_API_URL + 'api/puntos-conseguidos-user';
 
   constructor(protected http: HttpClient) {}
 
@@ -31,6 +34,12 @@ export class UserTeamsService {
     .pipe(map((res: EntityArrayResponseTypeObject) => this.convertDateArrayFromServer(res)));
   }
 
+  findPuntos(login: string): Observable<EntityArrayResponseTypePuntos> {
+    return this.http
+    .get<IPuntosConseguidos[]>(`${this.resourcePuntos}/${login}`, { observe: 'response' })
+    .pipe(map((res: EntityArrayResponseTypePuntos) => this.convertDateArrayFromServerPunts(res)));
+  }
+
   query(req?: any): Observable<EntityArrayResponseType> {
     const options = createRequestOption(req);
     return this.http.get<IUser[]>(this.resourceUrl, { params: options, observe: 'response' });
@@ -41,6 +50,15 @@ export class UserTeamsService {
       res.body.forEach((objetivosConseguidos: IObjetivosConseguidos) => {
         objetivosConseguidos.fechaApertura = objetivosConseguidos.fechaApertura ? moment(objetivosConseguidos.fechaApertura) : undefined;
         objetivosConseguidos.fechaCierre = objetivosConseguidos.fechaCierre ? moment(objetivosConseguidos.fechaCierre) : undefined;
+      });
+    }
+    return res;
+  }
+
+  protected convertDateArrayFromServerPunts(res: EntityArrayResponseType): EntityArrayResponseType {
+    if (res.body) {
+      res.body.forEach((puntosConseguidos: IPuntosConseguidos) => {
+        puntosConseguidos.anyos = puntosConseguidos.anyos ? moment(puntosConseguidos.anyos) : undefined;
       });
     }
     return res;
